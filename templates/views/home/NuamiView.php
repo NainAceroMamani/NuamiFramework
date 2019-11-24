34ff3c02abaf534090c8e9cba71b69e1d1dbcbb6
NainAceroMamani <55763187+NainAceroMamani@users.noreply.github.com>
2019-11-24 14:53:59 -0500

    Update NuamiView.php

--- templates/views/home/NuamiView.php
+++ templates/views/home/NuamiView.php
@@ -6,9 +6,9 @@
       <h2 class="text-white mt-5 mb-3"><span class="text-warning">Nuami</span> framework</h2>
       <p class="text-center text-white">Hay una niña que me deja impactado <br> Su suave y adorable nombre es Nuami ,  Una obra que Dios extrañamente ha expresado <br> En una tarde calurosa como un parasol.</p>
       <div class="mt-5">
-        <a class="btn btn-warning btn-lg" href="#">Probar</a>
-        <a class="btn btn-success btn-lg" href="#"><i class="fas fa-download"></i> Descargar</a>
-        <a class="btn btn-info btn-lg" href="#"><i class="fab fa-github"></i> Github</a>
+        <a class="btn btn-warning btn-lg" href="https://github.com/NainAceroMamani/NuamiFramework">Probar</a>
+        <a class="btn btn-success btn-lg" href="https://github.com/NainAceroMamani/NuamiFramework"><i class="fas fa-download"></i> Descargar</a>
+        <a class="btn btn-info btn-lg" href="https://github.com/NainAceroMamani/NuamiFramework"><i class="fab fa-github"></i> Github</a>
       </div>
       <div class="mt-5">
         <p class="text-muted">Desarrollado con Amor <i class="fas fa-heart text-danger"></i> </p>
